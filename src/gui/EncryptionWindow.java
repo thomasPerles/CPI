80270221ab10a2b02636f023c36914a92cd1546a
Tuture <natiora.aina@gmail.com>
2016-11-11 11:18:58 +0100

    Create encryption window

---
+++ src/gui/EncryptionWindow.java
@@ -0,0 +1,78 @@
+package gui;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class EncryptionWindow {
+
+	private JFrame frame;
+	private JTextField passwordTextField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EncryptionWindow window = new EncryptionWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EncryptionWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Encryption");
+		frame.setSize(300, 50);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel passwordLabel = new JLabel("Enter password :");
+		passwordLabel.setBounds(31, 83, 138, 36);
+		frame.getContentPane().add(passwordLabel);
+		
+		passwordTextField = new JTextField();
+		passwordTextField.setBounds(193, 83, 208, 36);
+		frame.getContentPane().add(passwordTextField);
+		passwordTextField.setColumns(10);
+		
+		JButton encryptButton = new JButton("Encrypt");
+		encryptButton.setBounds(251, 142, 105, 36);
+		frame.getContentPane().add(encryptButton);
+		
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setBounds(251, 189, 105, 36);
+		frame.getContentPane().add(cancelButton);
+		
+		cancelButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+		
+	}
+}
