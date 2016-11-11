b4216df4588a0e24846a16b0350aaa0c7b80041c
Tuture <natiora.aina@gmail.com>
2016-11-11 12:05:03 +0100

    #2 create panel for result after entering password

---
+++ src/gui/ResultPanel.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ResultPanel extends JPanel {
+
+	private JLabel resutlMessage;
+	
+	public ResultPanel(int i) {
+		super();
+	}
+	
+	
+}
