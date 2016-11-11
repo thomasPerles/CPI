63cb4f2bb7f437cccaffd2fd3c7ebd3babbd7b4f
thomas <thomas@thomas-X555LJ>
2016-11-11 15:03:34 +0100

    Seperating mains in ImageLoader

--- src/image_manipulation/ImageLoader.java
+++ src/image_manipulation/ImageLoader.java
@@ -1,10 +1,7 @@
 package image_manipulation;
 
-public class ImageLoader {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public abstract class ImageLoader {
+	
+	
+	
 }
---
+++ src/image_manipulation/ImageLoaderTL.java
@@ -0,0 +1,9 @@
+package image_manipulation;
+
+public class ImageLoaderTL extends ImageLoader {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
