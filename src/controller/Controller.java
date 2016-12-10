2196c4ab5602169db5e161ef9d0630c0daa2cd0c
thomas <thomas@thomas-X555LJ>
2016-12-10 14:24:09 +0100

    MVC Natiora

---
+++ src/controller/Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public abstract class Controller implements MouseListener, 
+			MouseMotionListener, ActionListener, KeyListener {}
