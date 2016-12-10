aeb440bf671dbadc1a665df64e7ce6cbb3760dc1
Myst3riis <flavien.everaert@gmail.com>
2016-12-10 16:16:29 +0100

    //PAS FINI\\ !! test read meta

---
+++ src/crypto/TestMetaWrite.java
@@ -0,0 +1,56 @@
+package crypto;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class TestMetaWrite
+{
+	public static void main(String[] args) throws IOException {
+		
+	}
+
+	void readAndDisplayMetadata( String fileName ) {
+        try {
+
+            File file = new File( fileName );
+            ImageInputStream iis = ImageIO.createImageInputStream(file);
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+
+            if (readers.hasNext()) {
+
+                // pick the first available ImageReader
+                ImageReader reader = readers.next();
+
+                // attach source to the reader
+                reader.setInput(iis, true);
+
+                // read metadata of first image
+                IIOMetadata metadata = reader.getImageMetadata(0);
+
+                String[] names = metadata.getMetadataFormatNames();
+                int length = names.length;
+                for (int i = 0; i < length; i++) {
+                    System.out.println( "Format name: " + names[ i ] );
+                    displayMetadata(metadata.getAsTree(names[i]));
+                }
+            }
+        }
+        catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+	
+	
+}
