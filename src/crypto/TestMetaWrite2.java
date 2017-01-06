61db4a1e8ca4952a37ab84587cf6f237d9dff39c
Myst3riis <flavien.everaert@gmail.com>
2017-01-06 17:42:39 +0100

    Hide zip in image for windows OK Hide zip in image for windows TEST

--- src/crypto/TestMetaWrite2.java
+++ src/crypto/TestMetaWrite2.java
@@ -1,24 +1,19 @@
 package crypto;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-
-import javax.imageio.ImageIO;
 
 public class TestMetaWrite2
 {
 	public void hideZipInImage(String imageFolder, String imageName) throws IOException{
 		//Creer un zip avec le json dedans
 		FileInputStream in = new FileInputStream(imageFolder+imageName);
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(imageFolder+"tmp.zip"));
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(imageFolder+"json.zip"));
         out.putNextEntry(new ZipEntry(imageName)); 
         byte[] b = new byte[1024];
         int count;
@@ -27,6 +22,9 @@ public class TestMetaWrite2
         }
         out.close();
         in.close();
+        
+        
+        //File imageFile = new File(imageFolder+imageName);
         
 		//QUEL OS ?
         	//Windows ?
@@ -38,16 +36,40 @@ public class TestMetaWrite2
         
     	String os = System.getProperty("os.name").toLowerCase();
 
-		System.out.println(os);
-
-		if ((os.indexOf("win") >= 0)) {
-			System.out.println("This is Windows");
-		} else if (os.indexOf("mac") >= 0) {
-			System.out.println("This is Mac");
-		} else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0 ) {
-			System.out.println("This is Unix or Linux");
-		} else if (os.indexOf("sunos") >= 0) {
-			System.out.println("This is Solaris");
+		if ((os.indexOf("win") >= 0)) { //WINDOWS
+			try {
+				ProcessBuilder builder = new ProcessBuilder(
+						"cmd.exe", "/c", "cd \""+imageFolder+"\" && copy /b "+imageName+"+json.zip "+imageName);
+			        builder.redirectErrorStream(true);
+			        Process p = builder.start();
+			        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			        String line;
+			        while (true) {
+			            line = r.readLine();
+			            if (line == null) { break; }
+			            //System.out.println(line);
+			        }
+			    } catch (Exception ex) {}
+		} else if (os.indexOf("mac") >= 0) { // MAC OS
+			
+		} else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0 ) { //UNIX-LINUX
+			try{
+				String command ="cd "+imageFolder+" && cat "+imageName+" json.zip > "+imageName;
+				Process p;
+				String s;
+				p = Runtime.getRuntime().exec(command);
+	            BufferedReader br = new BufferedReader(
+	                new InputStreamReader(p.getInputStream()));
+	            while ((s = br.readLine()) != null)
+	                System.out.println("line: " + s);
+	            p.waitFor();
+	            System.out.println ("exit: " + p.exitValue());
+	            p.destroy();
+				
+				
+			} catch (Exception ex) {}
+		} else if (os.indexOf("sunos") >= 0) { //SOLARIS
+			
 		} else {
 			System.out.println("Your OS is not support!!");
 		}
@@ -70,7 +92,8 @@ public class TestMetaWrite2
 	public static void main(String[] args) throws IOException{
 		//MAIN DE TEST !!
 		TestMetaWrite2 test = new TestMetaWrite2();
-		test.hideZipInImage("C:/Users/Flav/git/CPI/res-test/","bearbull.bmp");
+		//test.hideZipInImage("C:/Users/Flav/git/CPI/res-test/","TEST.bmp");
+		test.hideZipInImage("res-test/","TEST.bmp");
 		//test.findZipInImage("");
 	}
 	
